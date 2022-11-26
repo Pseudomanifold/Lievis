934e5f809e91e9bd3189fcaca08651cfc2b47ae2
Bastian Rieck <bastian@rieck.me>
2022-11-26 17:21:43 +0100

    Experimenting with different embeddings for orthogonal group

--- lievis/o.py
+++ lievis/o.py
@@ -11,6 +11,7 @@ import numba
 
 @numba.njit
 def pairwise_numba(X):
+    n = X.shape[0]
     D = np.zeros((n, n), dtype=np.float64)
     for i in range(n):
         for j in range(i+1, n):
@@ -21,19 +22,38 @@ def pairwise_numba(X):
 
 
 if __name__ == "__main__":
-    n = 500
-    d = 5
+    n = 50
+    d = 20
 
     rng = np.random.default_rng(42)
 
-    group = SpecialOrthogonal(n=d)
-    X = group.random_point(n)
+    X = []
+    z = []
+
+    for dim in range(2, d + 1):
+        group = SpecialOrthogonal(dim)
+        sample = group.random_point(n)
+
+        if dim != d:
+            sample = np.asarray([
+                np.block([
+                    [M, np.zeros((dim, d - dim))],
+                    [np.zeros((d - dim, dim)), np.eye(d - dim)]
+                ]) for M in sample
+            ])
+
+        X.append(sample)
+        z.append([dim] * n)
+
+    X = np.asarray(X)
+    X = X.reshape(-1, d, d)
     y = np.asarray([np.linalg.norm(M) for M in X])
+    z = np.asarray(z).ravel()
 
     emb = MDS(dissimilarity="precomputed")
     D = pairwise_numba(X)
     X_emb = emb.fit_transform(D)
 
-    plt.scatter(X_emb[:, 0], X_emb[:, 1], c=y)
+    plt.scatter(X_emb[:, 0], X_emb[:, 1], c=z)
     plt.colorbar()
     plt.show()
